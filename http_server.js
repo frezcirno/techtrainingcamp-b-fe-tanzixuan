120a542748a7bcd2009b23d673e29d2f7dbeea24
Frezcirno <2277861660@qq.com>
2020-07-27 16:09:13 +0800

    实现了后端接口转发 更改Koa配置使之符合前端history路由

--- http_server.js
+++ http_server.js
@@ -1,21 +1,45 @@
+const fs = require('fs')
 const Koa = require('koa')
-// const Router = require('koa-router')
+const Router = require('koa-router')
+const body = require('koa-bodyparser')
 const serve = require('koa-static')
-// const { render } = require('./dist/bundle_server')
+const compress = require('koa-compress')
+const cors = require('koa-cors')
+const { default: axios } = require('axios')
 
 const app = new Koa()
-// const router = new Router()
+const router = new Router()
 const kPort = 8000
 
-// app.use(router.routes())
-//   .use(router.allowedMethods())
-
-// router.get('/', async ctx => {
-//   ctx.set('Content-Type', 'text/html')
-//   ctx.body = render()
-// })
-
+app.use(cors())
+app.use(body())
+app.use(async (ctx, next) => { // history 中间件
+  const path = ['/search'] // 需要判断的路径
+  await next() // 等待请求执行完毕
+  if (ctx.response.status === 404) {
+    if (path.reduce((res, next) => (res || ctx.request.url.includes(next)), false)) { // 判断是否符合条件
+      ctx.type = 'text/html; charset=utf-8' // 修改响应类型
+      ctx.body = fs.readFileSync('./build/index.html') // 修改响应体
+    }
+  }
+})
 app.use(serve('./build'))
+app.use(compress({ threshold: 2048 }))
+app.use(router.routes()).use(router.allowedMethods())
+
+router.get('/api/search', async ctx => {
+  const { keyword, offset } = ctx.query
+  let res = await axios.get(`https://i.snssdk.com/search/api/study?keyword=${keyword || ''}&offset=${offset || 0}`)
+  ctx.set('Content-Type', 'application/json')
+  ctx.body = await res.data
+})
+
+router.get('/api/search/sug', async ctx => {
+  const { keyword } = ctx.query
+  let res = await axios.get(`https://i.snssdk.com/search/api/sug/?keyword=${keyword || ''}`)
+  ctx.set('Content-Type', 'application/json')
+  ctx.body = await res.data
+})
 
 app.listen(kPort, () => {
   console.log('listening on:', kPort, 'pid:', process.pid)
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export async function searchsug(keyword) {
-    let res = await axios.get(`https://i.snssdk.com/search/api/sug/?keyword=${keyword}`)
+    let res = await axios.get(`/api/search/sug?keyword=${keyword}`)
     return await res.data
 }
 
 export async function search(keyword, offset) {
-    let res = await axios.get(`https://i.snssdk.com/search/api/study?keyword=${keyword}&offset=${offset}`)
+    let res = await axios.get(`/api/search?keyword=${keyword}&offset=${offset}`)
     return await res.data.data
 }
